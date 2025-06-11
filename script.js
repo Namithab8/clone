5551d5c789dcbe2ff74a3b572425f31e3f1a236d
Meghana-HS <eng22cs0103@dsu.edu.in>
2025-06-11 20:57:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let para=document.querySelector("p");
+// console.log(para);
+
+// let divs=document.querySelectorAll(".box");
+
+// let idx=1;
+// for(div of divs)
+// {
+//     div.innerText=`new value: ${idx}`;
+//     idx++;
+// }
+// let divs=document.querySelectorAll("div");
+// for(div of divs)
+// {
+//    let bb=div.getAttribute("class");
+//    console.log(bb);
+// }
+
